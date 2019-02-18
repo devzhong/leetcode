5537bd2f1ed66d2c40d599dce5a9fd4fd7ffee18
devzhong <devzhong@163.com>
2019-02-18 09:31:44 +0800

    Initial commit

---
+++ src/main/java/com/HelloWord.java
@@ -0,0 +1,13 @@
+package com;
+
+/**
+ * @Description:
+ * @Author: devzhong
+ * @Date: 2019/2/18 9:28
+ */
+public class HelloWord {
+
+    public static void main(String[] args){
+        System.out.println("hello world!");
+    }
+}
